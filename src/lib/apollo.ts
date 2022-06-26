c43007490d8154f4e040f94e2f3b307ce17e818a
gabrielgpena793 <kbeca.gabriel793@gmail.com>
2022-06-25 22:22:03 -0300

    aula 04

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -13,7 +13,10 @@ const defaultOptions: DefaultOptions = {
 };
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ocvp6y1trp01z7gtnweu4e/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACESS_TOKEN}`
+  },
   cache: new InMemoryCache(),
   defaultOptions: defaultOptions,
 })
